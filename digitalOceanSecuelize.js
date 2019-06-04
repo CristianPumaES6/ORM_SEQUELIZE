37ea5c41d57b629fb37c46bd83fc6155ba16e9d7
CristianPumaES6 <cpuma@inconcertcc.com>
2019-06-04 02:25:32 -0500

    Se intento hacer coneccion con sequelize pero fallo.

---
+++ digitalOceanSecuelize.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+const postgres = require('pg');
+
+//
+
+const sequelize = new Sequelize('cpuma', 'cpuma', 'Peru123.', {
+    // gimme postgres, please!
+    dialect: 'postgres',
+    host: '165.22.203.55',
+    ssl: true
+  })
+
+
+// probar la conexion
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
